250af22d46217fee8818501e1b134882e42b5dff
Yon Seungik <dongja94@gmail.com>
2016-07-22 17:51:02 +0900

    view animation

--- app/src/main/java/com/begentgroup/sampleanimation/MainActivity.java
+++ app/src/main/java/com/begentgroup/sampleanimation/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
